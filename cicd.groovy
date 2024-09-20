17d668027e3ee8ae45ce67448b329e806c58f964
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:57 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/patchport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/patchport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/patchport.git'), string(name: 'PORT_DESCRIPTION', value: 'Patch takes a patch file containing a difference listing produced by the diff program and applies those differences to one or more original files, producing patched versions.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/patchport.git'), string(name: 'PORT_DESCRIPTION', value: 'Patch takes a patch file containing a difference listing produced by the diff program and applies those differences to one or more original files, producing patched versions.' )]
   }
 }
